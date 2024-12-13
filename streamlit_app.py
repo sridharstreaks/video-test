0afdcc511e97e338a0a70613d5e80ece2e38a996
Sridhar Streaks <100800059+sridharstreaks@users.noreply.github.com>
2024-12-13 15:55:49 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+
+
+st.video("https://dl5.hotshare.click/Family_Padam_2024_PreDVD_360p_HD.mp4",format="video/mp4", start_time=0, *, subtitles=None, end_time=None, loop=False, autoplay=False, muted=False)
