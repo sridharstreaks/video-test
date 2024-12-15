5869679000e00f4706f38eb480f03b3198a9db98
Sridhar Streaks <100800059+sridharstreaks@users.noreply.github.com>
2024-12-15 20:00:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 
 
-st.video("https://cdn5.gotofiles.online/download/4d21b3c87970e00398b1ca232bae7530/1734273639/p/Tamil%20HD%20Mobile%20Movies/Ananthapurathu%20Veedu%20(2010)/Ananthapurathu%20Veedu%20(Original)/Ananthapurathu%20Veedu%20(360p%20HD)/Ananthapurathu%20Veedu%202010%20HD.mp4",format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=False, autoplay=False, muted=False)
+st.video("https://cdn5.gotofiles.online/download/4d21b3c87970e00398b1ca232bae753…ginal)/Ananthapurathu Veedu (360p HD)/Ananthapurathu Veedu 2010 HD.mp4",format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=False, autoplay=False, muted=False)
