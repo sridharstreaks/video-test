66b7d22394bf1e19b82ec63c9df946ba9e23081e
Sridhar Streaks <100800059+sridharstreaks@users.noreply.github.com>
2024-12-15 19:22:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 
 
-st.video("https://dl.vdocdn.com/openload/1c48e7ca6aca9e606e60bf1cfb9c9d10/4b68c65a53f42fba04c6dab4d4c9adb820241212150153/0baac0/97f809/1734270891?dl=1",format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=False, autoplay=False, muted=False)
+st.video("https://dl.vdocdn.com/openload/1c48e7ca6aca9e606e60bf1cfb9c9d10/4b68c65a53f42fba04c6dab4d4c9adb820241212150153/0baac0/97f809/1734270891?dl=1.mp4",format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=False, autoplay=False, muted=False)
