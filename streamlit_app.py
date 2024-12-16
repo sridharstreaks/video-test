83ede3b6bb5bdf6a296373301706cd0afde6e353
Sridhar Streaks <100800059+sridharstreaks@users.noreply.github.com>
2024-12-16 14:52:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,23 +3,33 @@ import os
 import random
 import time
 import requests
-from lxml import html
+from lxml import HTML
 
-def sample_while():
-    i=0
-    last=''
-    d ={}
-    whi not any("Ukraine" in value or "America" in value for value in d.values()):
-        os.write(1, ".mp4 not found. Continuing the loop.\n".encode())
-        if len(d) > 0:
-            *_, last = d.values()
-            os.write(1, f"{last}\n".encode())
-        response=requests.get("https://randomuser.me/api/")
-        tree = html.fromstring(response.content)
-        d[i]=tree.xpath('//pre/text()')
-        i+=1
+d={}
+
+def get_domain(i):
+    response=requests.get("https://randomuser.me/api/")
+    tree = html.fromstring(response.content)
+    d[i]=tree.xpath('//text()')
+    time.sleep(random.randint(0,3))
     return d
 
+def count_characters_in_dict_values(my_dict):
+    total_count = 0
+    for value in my_dict.values():
+        if isinstance(value, str):  # Check if the value is a string
+            total_count += len(value)
+        time.sleep(10)
+    return total_count
+
+def display_result(result):
+    return result/2
+    
+
+result = get_domain(2)
+
 with st.spinner("Running function..."):
-        result = sample_while()
-        os.write(1, f"download_link_fetcher_result: {result}\n".encode())
+        result_2 = count_characters_in_dict_values(result)
+
+result_3 = display_result(result_2)
+os.write(1, f"download_link_fetcher_result: {result_3}\n".encode())
