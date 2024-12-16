b1573253d22818aa8f69cb82456d70de18af9d91
Sridhar Streaks <100800059+sridharstreaks@users.noreply.github.com>
2024-12-16 10:21:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import random
 import streamlit as st
 import os
 import asyncio
+import concurrent.futures
 
 payload = {
 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
@@ -32,7 +33,6 @@ def get_domian(search_keyword,as_sitesearch=None):
             dicto[search_keyword]=movie_url
         return dicto
         
-@st.cache_data
 async def download_link_fetcher(dicto):
     while not any(".dl" in value or ".mp4" in value for value in dicto.values()):
         os.write(1, ".mp4 not found. Continuing the loop.\n".encode())
@@ -78,14 +78,13 @@ if st.button("Submit"):
     get_domian_result = get_domian(input_text,"3moviesda.com")
     os.write(1, f"{get_domian_result}\n".encode())
 
-    with st.spinner("Running function..."):
-         # Trigger second function with the result of the first
-        try:
-            download_link_fetcher_result = asyncio.run(download_link_fetcher(get_domian_result))
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(download_link_fetcher(get_domian_result))
+
+        with st.spinner("Running function..."):
+            download_link_fetcher_result = future.result()
             os.write(1, f"download_link_fetcher_result: {download_link_fetcher_result}\n".encode())
-        except Exception as e:
-            st.error(f"Error in download_link_fetcher: {str(e)}")
-            st.stop()
+         # Trigger second function with the result of the first
 
     # Final streaming link extractor
     try:
